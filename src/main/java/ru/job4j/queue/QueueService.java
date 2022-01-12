38da51bce9f3ef1a7e7f65fde7d3963812042c02
ftptpf <inboxru@yandex.ru>
2022-01-12 09:54:30 +0300

    2. Тестовое задание - проект "Pooh JMS" [#268841] - QueueService

--- src/main/java/ru/job4j/queue/QueueService.java
+++ src/main/java/ru/job4j/queue/QueueService.java
@@ -4,14 +4,34 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentMap;
 
+/**
+ * Отправитель посылает запрос на добавление данных с указанием очереди (weather) и значением параметра (temperature=18).
+ * Сообщение помещается в конец очереди. Если очереди нет в сервисе, то нужно создать новую и поместить в нее сообщение.
+ *
+ * Получатель посылает запрос на получение данных с указанием очереди. Сообщение забирается из начала очереди и удаляется.
+ * Если в очередь приходят несколько получателей, то они поочередно получают сообщения из очереди.
+ * Каждое сообщение в очереди может быть получено только одним получателем.
+ */
 public class QueueService implements Service {
     private final ConcurrentMap<String, ConcurrentLinkedQueue<String>> queue = new ConcurrentHashMap<>();
 
     @Override
     public Resp process(Req req) {
         String text = "";
-        String status = "";
-
+        String status = "204";
+        String type = req.httpRequestType();
+        String key = req.getSourceName();
+        String param = req.getParam();
+        if ("POST".equals(type)) {
+            /* Если нет значения для соответствующего ключа - создаем новое значение (очередь) */
+            queue.putIfAbsent(key, new ConcurrentLinkedQueue<>());
+            /* Добавляем в очередь полученный параметр */
+            queue.get(key).add(param);
+        } else if ("GET".equals(type)) {
+            /* Извлекаем сообщение из начала очереди при этом удаляя его */
+            text = queue.get(key).poll();
+            status = "200";
+        }
         return new Resp(text, status);
     }
 }
--- src/main/java/ru/job4j/queue/TopicService.java
+++ src/main/java/ru/job4j/queue/TopicService.java
@@ -1,15 +1,41 @@
 package ru.job4j.queue;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentMap;
 
+/**
+ * Отправитель посылает запрос на добавление данных с указанием топика (weather) и значением параметра (temperature=18).
+ * Сообщение помещается в конец каждой индивидуальной очереди получателей.
+ * Если топика нет в сервисе, то данные игнорируются.
+ *
+ * Получатель посылает запрос на получение данных с указанием топика. Если топик отсутствует, то создается новый.
+ * А если топик присутствует, то сообщение забирается из начала индивидуальной очереди получателя и удаляется.
+ * Когда получатель впервые получает данные из топика – для него создается индивидуальная пустая очередь.
+ * Все последующие сообщения от отправителей с данными для этого топика помещаются в эту очередь тоже.
+ * Таким образом в режиме "topic" для каждого потребителя будет своя уникальная очередь с данными,
+ * в отличие от режима "queue", где для все потребители получают данные из одной и той же очереди.
+ */
 public class TopicService implements Service {
     private final ConcurrentMap<String, ConcurrentHashMap<String, ConcurrentLinkedQueue<String>>> topic =
             new ConcurrentHashMap<>();
 
     @Override
     public Resp process(Req req) {
-        return null;
+        String text = "";
+        String status = "204";
+        String type = req.httpRequestType();
+        String key = req.getSourceName();
+        String param = req.getParam();
+        if ("POST".equals(type)) {
+            /* Добавляем значение параметра в конец каждой индивидуальной очереди */
+            topic.get(key).forEachValue(2, value -> value.add(param));
+        } else if ("GET".equals(type)) {
+            /* Если нет значения для соответствующего ключа - создаем новый топик */
+            topic.putIfAbsent(key, new ConcurrentHashMap<>());
+        }
+
+        return new Resp(text, status);
     }
 }
